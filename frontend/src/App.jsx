4a2ee208ebd9d3e42dd598e54dc985239af9e050
Ganza Bruce <ganzabruce@MacBook-Pro-Ganza.local>
2025-04-16 15:13:14 +0200

    successfully implemented the login and logout mechanisms

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Home from './pages/home'
 import Login from './pages/login'
 import Signup from './pages/signup'
-
+import { useLogout } from './hooks/useLogout'
 
 
 function App() {
-
+  const {logout} = useLogout()
+  const handleClick = () =>{
+    logout()
+  }
   return (
     <Router>
         <main>
+          <button onClick={handleClick}>logout</button>
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/login' element={<Login />} />
